e88d8fc71472949a015191041b7eb5e0b17b333a
Salmanda <93958600+SalmandaAK@users.noreply.github.com>
2025-02-07 09:30:12 +0700

    Added configure currency command

---
+++ cmd/configure.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/SalmandaAK/expense-tracker/internal/view"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Configure currency shown",
+	Long: `
+	Configure lets you type the currency you are using. Example usage:
+	./expense-tracker configure --currency IDR
+	The default is $
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		viper.Set("currency", currency)
+		err := viper.WriteConfig()
+		if err != nil {
+			view.DisplayError(err)
+			return
+		}
+		view.DisplayMessage(fmt.Sprintf("Currency has been changed successfully into %s", currency))
+	},
+}
+
+var currency string
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+
+	configureCmd.Flags().StringVar(&currency, "currency", "", "Change currency")
+	configureCmd.MarkFlagRequired("currency")
+}
